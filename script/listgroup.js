3b00f6e5c17e3badfe9ffe7d000bd22c335cb31e
Galde <federico.galdenzi@studio.unibo.it>
2018-12-30 18:25:54 +0100

    edit somethings in foodInsertForm.html and tables.js

--- script/listgroup.js
+++ script/listgroup.js
@@ -3,17 +3,31 @@ $(document).ready(function() {
 	/*quando clicco sul bottone con id = #btnInsertIngredient, creo un nuovo elemento con il testo dell'input text con id = #defaultIngredientFood
 	  e lo aggiungo alla list-group.
 	*/
+	/*
 	$("#btnInsertIngredient").click(function () {
 		var cl = "py-1 list-group-item list-group-item-action";
 		var newEl = $('<a id="item-list-group"></a>').addClass(cl).text($("#defaultIngredientFood").val());
 		$(".list-group").append(newEl);
 	});
+	*/
 	/*
 		vado ad agganciare un evento click su un elemento che ancora non è stato creato, semplicemente restituisce il testo dell'elemento nella list group
 		e lo mette all'interno della input text con id = #defaultIngredientFood.
 	*/
 	$("body").on('click', '.list-group-item', function () {
-		$("#defaultIngredientFood").val($(this).text());
+		var textTag = $(this).text() + ";";
+		if ($(this).hasClass("active")) {
+			$(this).removeClass("active");
+			$("#list-group-tags").children("input").val(function (i,val) {
+				return val.replace(textTag, "");
+			});
+		} else {
+			$(this).addClass("active");
+			$("#list-group-tags").children("input").val(function(i, val) {
+				return val + textTag;
+			});
+		}
+		console.log($("#list-group-tags").children("input").val());
 	});
 	
 	
