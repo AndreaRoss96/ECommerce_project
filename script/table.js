d712c0449f9faaba23ab2f78c619fce039b08d06
Galde <federico.galdenzi@studio.unibo.it>
2018-12-30 19:40:40 +0100

    changes in inserFoodForm.html, TO-DO: php part for add, edit and delete; also need to do the table with the previous and next;

--- script/table.js
+++ script/table.js
@@ -10,22 +10,22 @@ $(document).ready(function () {
 			//aggiungo la classe highlight all'elemento selezionato.
 			$(this).addClass("bg-info");
 			var tdEls = $(this).children("td");
-			var tags = tdEls.eq(3).val().split(';');
-			console.log(tags);
+			
+			var tags = tdEls.eq(2).text().split(";");
 			var i = 0;
 			var j = 0;
 			var headerRow;
-			$("#headerRow").children("th").each(function (i) {2
+			$(".list-group").children().removeClass("active");
+			$("#headerRow").children("th").each(function (i) {
 				j = 0;
 				if (i > 0) {
 					headerRow = "#default" + $(this).text().replace(" ","");
-					if (headerRow.includes("TagFood") {
+					if (headerRow.includes("TagFood")) {
 						while(j < tags.length) {
-							$(".list-group").children("#defaultTagFood:contains(" + tags.eq(j).text() + ")").addClass("active");
+							$(".list-group").children("#defaultTagFood:contains(" + tags[j].trim() + ")").addClass("active");
 							j++;
 						}
 					} else {
-						console.log("diocane");
 						$("body").find(headerRow).val(tdEls.eq(i - 1).val());
 					}
 				}
