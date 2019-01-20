ca15f790dd6ad01db7d0a9498f127c0369dd4ad8
Andrea Rossolini <andrea.rossolini2@studio.unibo.it>
2019-01-20 16:04:04 +0100

    Cart information

--- html/jquery/getNav.js
+++ html/jquery/getNav.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
       });
         jQuery.ajax({
           type: "POST",
-          url: "../php/loginInformations.php", 
+          url: "../php/loginInformations.php",
           dataType: "json",
           success: function (response)
-          { var userInfoCollapse = "<div class='collapse' id='userInfoCollapse'>"
+          {
+            var userInfoCollapse = "<div class='collapse' id='userInfoCollapse'>"
                                     +"<div id='userInfo' class='card card-body'>";
-                              
+
             $.each(response,function(key,val){
               userInfoCollapse = userInfoCollapse + "<p>" + key + " : " + val + "</p>";
             });
-                            
+
               userInfoCollapse = userInfoCollapse +"</div>" + "</div>";
               $("nav").after(userInfoCollapse);
               var button = $("#userButton");
@@ -23,7 +24,7 @@ $(document).ready(function(){
               button.attr("href","#userInfoCollapse");
               button.attr("role","button");
               button.attr("aria-expanded","false");
-              button.attr("aria-controls","userInfoCollapse"); 
+              button.attr("aria-controls","userInfoCollapse");
               var logoutButton = "<form action='../php/logout.php'>"
                                 +"<button type='submit' class='btn btn-info'>Logout</button>"
                                 +" </form>";
@@ -33,9 +34,9 @@ $(document).ready(function(){
               $("#userInfo > p").last().after(changePasswordButton);
               $("#userInfo > p").last().after(logoutButton);
             //alert(userInfoCollapse);
-            
+
           },
-        
+
           error:function (xhr, ajaxOptions, thrownError)
         {
           var button = $("#userButton");
@@ -44,7 +45,17 @@ $(document).ready(function(){
           button.removeAttr("data-toggle","collapse")
           button.removeAttr("role","button");
           button.removeAttr("aria-expanded","false");
-          button.removeAttr("aria-controls","userInfoCollapse"); 
+          button.removeAttr("aria-controls","userInfoCollapse");
+        }
+      });
+
+      jQuery.ajax({
+        url: "../php/cartInformations.php",
+        dataType: "json",
+        success: function(response) {
+          var userInfoCollapse = "<div class='collapse' id='cartInfoCollapse'>"
+                                  +"<div id='cartInfo' class='card card-body'>";
+
         }
       });
 });
