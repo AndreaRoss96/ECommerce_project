5f928bf26325f3b2584da4e6f68ebd8daea22bf3
Galde <federico.galdenzi@studio.unibo.it>
2019-01-29 00:24:15 +0100

    staph, pls d|_|la|\|

--- php/functionsGalde.php
+++ php/functionsGalde.php
@@ -22,12 +22,12 @@
 					$str = str_replace("#", "restaurantList.php?tag=" . $row["id"], $str);
 					$result2 = $conn->query('SELECT COUNT(DISTINCT ristP_IVA) AS num FROM tagportata WHERE idTag = ' . $row["id"]);
 					$count = $result2->fetch_assoc()['num'];
-					$str .= '  <span class="badge badge-primary badge-pill">' . $count . '</span>';
+					$str .= '  <span class="badge badge-success badge-pill">' . $count . '</span>';
 				}
 				echo $str . '</a>';
 			}
 		} else {
-			echo 'error table database or query.';
+			//echo 'error table database or query.';
 		}
 	}
 	
@@ -67,7 +67,7 @@
 			else{
 				$image = '../res/noProfileImage.jpg';
 			}
-			$str = '<div id="restaurantCard" class="col-sm mb-2"> <div class="card" style="width: 18rem;"> <img class="card-img-top" src='.$image.' alt="Immagine ristorante"> <div class="card-body"> <h5 class="card-title">' . $row["nomeRistorante"] . '</h5> <p class="card-text">' . $row["descrizione"] . '</p> <a href="resturant.php?p_iva=' . $row["P_IVA"] . '" class="btn btn-primary">Visita</a> </div> </div> </div>';
+			$str = '<div id="restaurantCard" class="col-sm mb-2"> <div class="card" style="width: 18rem;"> <img class="card-img-top" src='.$image.' alt="Immagine ristorante"> <div class="card-body"> <h5 class="card-title">' . $row["nomeRistorante"] . '</h5> <p class="card-text">' . $row["descrizione"] . '</p> <a href="resturant.php?p_iva=' . $row["P_IVA"] . '" class="btn btn-success">Visita</a> </div> </div> </div>';
 			if ($i % 2 == 0 && $i != 0) {
 				$str = '</div> <div class="row"> <div class="col-sm"></div>' . $str;
 			}
